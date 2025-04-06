2bfdd43341a53e3fdb0653cf7f5124d424d562f9
Alexander Novikov <nas133@gmail.com>
2025-04-06 19:13:25 -0400

    100Hz

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+
+#define NOP __asm__ ("nop\n\t")
+#define NOP5 __asm__("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\t")
+#define NOP10 __asm__("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t")
+
+#define TAXI_EXT_TRIGGER  13
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(TAXI_EXT_TRIGGER, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int period = 10; //random(1, 20);
+  digitalWriteFast(TAXI_EXT_TRIGGER, HIGH);
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  NOP10;
+  digitalWriteFast(TAXI_EXT_TRIGGER, LOW);
+  delay(period);
+}
+
